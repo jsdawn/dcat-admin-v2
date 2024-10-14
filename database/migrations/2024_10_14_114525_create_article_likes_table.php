5e68d663f1018e22b67a8208e36d03f17743863a
jsdawn <kingwyh1993@163.com>
2024-10-14 11:50:01 +0800

    add article like table migrations

---
+++ database/migrations/2024_10_14_114525_create_article_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_likes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->comment('用户id');
+            $table->bigInteger('article_id')->nullable()->comment('文章id');
+            $table->bigInteger('comment_id')->nullable()->comment('评论id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_likes');
+    }
+}
